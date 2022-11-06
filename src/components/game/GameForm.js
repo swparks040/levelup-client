9226032ddc5e860204f4d74f74e60bb849f5731b
Scott Parks <swparks040@gmail.com>
2022-11-06 09:55:46 -0600

    chapter 9 complete

---
+++ src/components/game/GameForm.js
@@ -0,0 +1,105 @@
+import { useState, useEffect } from "react"
+import { useNavigate } from 'react-router-dom'
+import { createGame, getGameTypes } from '../../managers/GameManager.js'
+
+
+export const GameForm = () => {
+    const navigate = useNavigate()
+    const [gameTypes, setGameTypes] = useState([])
+
+    /*
+        Since the input fields are bound to the values of
+        the properties of this state variable, you need to
+        provide some default values.
+    */
+    const [currentGame, setCurrentGame] = useState({
+        skillLevel: 1,
+        numberOfPlayers: 0,
+        title: "",
+        maker: "",
+        gameTypeId: 0
+    })
+
+    useEffect(() => {
+        // TODO: Get the game types, then set the state
+    }, [])
+
+    const changeGameState = (domEvent) => {
+        // TODO: Complete the onChange function
+    }
+
+    return (
+        <form className="gameForm">
+            <h2 className="gameForm__title">Register New Game</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="title">Title: </label>
+                    <input type="text" name="title" required autoFocus className="form-control"
+                        value={currentGame.title}
+                        onChange={changeGameState}
+                    />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="maker">Maker: </label>
+                    <input type="text" name="maker" required autoFocus className="form-control"
+                        value={currentGame.maker}
+                        onChange={changeGameState}
+                    />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="number_of_players">Number of Players: </label>
+                    <input type="text" name="number_of_players" required autoFocus className="form-control"
+                        value={currentGame.number_of_players}
+                        onChange={changeGameState}
+                    />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="skill_level">Skill Level: </label>
+                    <input type="text" name="skill_level" required autoFocus className="form-control"
+                        value={currentGame.skill_level}
+                        onChange={changeGameState}
+                    />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                <select
+                    value={game_type.id}
+                    onChange={changeGameState}>
+                    <option value="0">Choose...</option>
+                    {
+                        game_type.map(e => <option key={`game_type--${e.id}`} value={e.id}>{e.full_name}</option>)
+                    }
+                </select>
+                </div>
+            </fieldset>
+
+            {/* TODO: create the rest of the input fields */}
+
+            <button type="submit"
+                onClick={evt => {
+                    // Prevent form from being submitted
+                    evt.preventDefault()
+
+                    const game = {
+                        maker: currentGame.maker,
+                        title: currentGame.title,
+                        number_of_players: parseInt(currentGame.numberOfPlayers),
+                        skill_level: parseInt(currentGame.skillLevel),
+                        game_type: parseInt(currentGame.gameTypeId)
+                    }
+
+                    // Send POST request to your API
+                    createGame(game)
+                        .then(() => navigate("/games"))
+                }}
+                className="btn btn-primary">Create</button>
+        </form>
+    )
+}
--- src/managers/GameManager.js
+++ src/managers/GameManager.js
@@ -6,3 +6,17 @@ export const getGames = () => {
     })
         .then(response => response.json())
 }
+
+export const getGameTypes = () => {
+    return fetch("http://localhost:8000/game_types", { 
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        } 
+    })
+        .then(response => response.json())
+}
+
+export const createGame = (game) => {
+    return fetch("http://localhost:8000/games", { })
+        .then()
+}
